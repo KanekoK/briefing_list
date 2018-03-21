52089828607e365e4e472e22dda8a3220c3a1d71
kenya <kenya@ootaterumi-no-MacBook-Air.local>
2018-03-22 07:27:14 +0900

    fist commit

---
+++ main.py
@@ -0,0 +1,91 @@
+import urllib.request
+import openpyxl as px
+from bs4 import BeautifulSoup
+
+# URLの設定
+url = "https://job.rikunabi.com/2019/search/seminar/result/?dd=20180403&dd=20180404&dd=20180405&dd=20180406&k=11&k=12&k=13&k=14&pn="
+# 媒体の設定
+media = "リクナビ"
+
+
+
+def access(url, cnt):
+	html = urllib.request.urlopen(url+str(cnt))
+	soup = BeautifulSoup(html, "html.parser")
+	if soup.select("._vacantSeat ._vacantSeat-data-day"):
+		return soup
+	else:
+		return False
+
+def get_info_list(soup):
+	event_list   = []
+	area_list    = []
+	company_list = []
+	link_list    = []
+	event_date = soup.select("._vacantSeat ._vacantSeat-data-day")
+	area = soup.select("._vacantSeat ._vacantSeat-data-place")
+	company_name = soup.select(".search-cassette .search-cassette-title a")
+	link = soup.select(".search-cassette-footer .search-cassette-actionBar-cell_04 a.mod-btn")
+
+	for s in event_date:
+		event_list.append(s.string)
+	for s in area:
+		area_list.append(s.string.replace(" ", "").replace("\n", ""))
+	for s in company_name:
+		company_list.append(s.string)
+	for s in link:
+		link_list.append("https://job.rikunabi.com" + s.get("href"))
+	return event_list, area_list, company_list, link_list
+
+def insert_info_list(soup, event_list, area_list, company_list, link_list):
+	event_date = soup.select("._vacantSeat ._vacantSeat-data-day")
+	area = soup.select("._vacantSeat ._vacantSeat-data-place")
+	company_name = soup.select(".search-cassette .search-cassette-title a")
+	link = soup.select(".search-cassette-footer .search-cassette-actionBar-cell_04 a.mod-btn")
+
+	for event in event_date:
+		for s in event:
+			event_list.append(s.string)
+	for areas in area:
+		for s in areas:
+			area_list.append(s.string.replace(" ", "").replace("\n", ""))
+	for s in company_name:
+		company_list.append(s.string)
+	for s in link:
+		link_list.append("https://job.rikunabi.com" + s.get("href"))
+	return event_list, area_list, company_list, link_list
+
+def make_assoc(event_list, area_list, company_list, link_list):
+	info = {}
+	sheet, book = excel_sheet("説明会一覧.xlsx", "Sheet1")
+	for i in range(len(event_list)):
+		sheet.cell(row=i+2, column=1, value="リクナビ")
+		sheet.cell(row=i+2, column=2, value=event_list[i])
+		sheet.cell(row=i+2, column=3, value=area_list[i])
+		sheet.cell(row=i+2, column=4, value=company_list[i])
+		sheet.cell(row=i+2, column=5, value=link_list[i])
+		sheet.cell(row=i+2, column=5, value=link_list[i]).hyperlink = link_list[i]
+	book.save('説明会一覧.xlsx')
+
+def excel_sheet(filepath, sheetname):
+	book = px.load_workbook(filepath)
+	sheet = book.active
+	return sheet, book
+
+if __name__ == '__main__':
+	flag = True
+	cnt = 1
+	soup = access(url, cnt)
+	event_list, area_list, company_list, link_list = get_info_list(soup)
+
+	while flag:
+		cnt += 1
+		soup = access(url, cnt)
+		if soup == False:
+			flag = False
+		else:
+			event_list, area_list, company_list, link_list = insert_info_list(soup, event_list, area_list, company_list, link_list)
+
+	make_assoc(event_list, area_list, company_list, link_list)
+	print("完了！")
+
