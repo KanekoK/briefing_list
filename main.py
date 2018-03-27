a33804a8f3ee61d6cf474caa1631a2a4914d68ff
Kenya <b1r2d3q4@gmail.com>
2018-03-27 09:59:53 +0900

    .

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import openpyxl as px
 from bs4 import BeautifulSoup
 
 # URLの設定
-url = "https://job.rikunabi.com/2019/search/seminar/result/?dd=20180403&dd=20180404&dd=20180405&dd=20180406&k=11&k=12&k=13&k=14&pn="
+url = "https://job.rikunabi.com/2019/search/seminar/result/?dd=20180406&k=11&k=12&k=13&k=14&pn="
+# Sheet名の設定
+sheet_name = "4月6日"
+
 # 媒体の設定
 media = "リクナビ"
 
@@ -43,12 +46,10 @@ def insert_info_list(soup, event_list, area_list, company_list, link_list):
 	company_name = soup.select(".search-cassette .search-cassette-title a")
 	link = soup.select(".search-cassette-footer .search-cassette-actionBar-cell_04 a.mod-btn")
 
-	for event in event_date:
-		for s in event:
-			event_list.append(s.string)
-	for areas in area:
-		for s in areas:
-			area_list.append(s.string.replace(" ", "").replace("\n", ""))
+	for s in event_date:
+		event_list.append(s.string)
+	for s in area:
+		area_list.append(s.string.replace(" ", "").replace("\n", ""))
 	for s in company_name:
 		company_list.append(s.string)
 	for s in link:
@@ -58,6 +59,7 @@ def insert_info_list(soup, event_list, area_list, company_list, link_list):
 def make_assoc(event_list, area_list, company_list, link_list):
 	info = {}
 	sheet, book = excel_sheet("説明会一覧.xlsx", "Sheet1")
+	header_setting(sheet)
 	for i in range(len(event_list)):
 		sheet.cell(row=i+2, column=1, value="リクナビ")
 		sheet.cell(row=i+2, column=2, value=event_list[i])
@@ -67,9 +69,17 @@ def make_assoc(event_list, area_list, company_list, link_list):
 		sheet.cell(row=i+2, column=5, value=link_list[i]).hyperlink = link_list[i]
 	book.save('説明会一覧.xlsx')
 
+def header_setting(sheet):
+	sheet['A1'].value = '媒体'
+	sheet['B1'].value = '日付'
+	sheet['C1'].value = 'エリア'
+	sheet['D1'].value = '会社名'
+	sheet['E1'].value = '説明会リンク'
+
 def excel_sheet(filepath, sheetname):
 	book = px.load_workbook(filepath)
-	sheet = book.active
+	sheet = book.create_sheet(title=sheet_name)
+	# sheet = book.active
 	return sheet, book
 
 if __name__ == '__main__':
